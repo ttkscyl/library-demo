534ac9cdb326ee8e8507dfe036554fd0d3fd1a43
Yilang Chen <ttkscyl@gmail.com>
2025-02-11 10:01:02 +0000

    account

--- account.php
+++ account.php
@@ -3,11 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Account</title>
+    <title>My account | Library</title>
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- Oxanium font --> <!-- Oxanium font --> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&display=swap" rel="stylesheet">
+    
+    <!-- BS5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+      
 </head>
-<body>
-    <?php
 
-    ?>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="">
+            <img src="pictures/icon.png" alt="company logo" style="width:45px;" class="rounded-pill"> 
+            <span><b>&nbsp;&nbsp; Helios's Bookshelf</b></span>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="library.php">View books</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="copyright.html">Copyright</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="account.php">My account</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  
+    
 </body>
+
 </html>
