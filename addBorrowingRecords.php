49d804fdae0df8734a1ab58a55bae226f3cf762f
https-helios <guan.s@oundleschool.org.uk>
2025-02-11 10:10:02 +0000

    hi

--- addBorrowingRecords.php
+++ addBorrowingRecords.php
@@ -4,10 +4,12 @@ include_once("connection.php");
 array_map("htmlspecialchars", $_POST);
 print_r($_POST);
 $statement = $conn->prepare("INSERT INTO BorrowingRecords(RecordID, UserID, BookID, BorrowDate, DueDate, ReturnDate, Status)
- VALUES (:RecordID, :UserID, :BookID, :BorrowDate, :DueDate, :ReturnDate, Status)"); #add all fields seen on my admin
-$statement->bindParam(":RecordID", $_POST["Books"]);
+ VALUES (NULL, :UserID, :BookID, :BorrowDate, :DueDate, null, null)"); #add all fields seen on my admin
+
 $statement->bindParam(":UserID", $_POST["Users"]);
-$statement->bindParam(":BookID", $_POST["BookID"]);
-$statement->bindParam(":BorrowDate", $date);
+$statement->bindParam(":BookID", $_POST["Books"]);
+$statement->bindParam(":BorrowDate", $_POST["date"]);
+$statement->bindParam(":DueDate", $_POST["date"]);
+
 $statement->execute();
 ?>
